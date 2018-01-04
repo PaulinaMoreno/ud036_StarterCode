062d3d810b862da02117be015a333a001efc6c89
Paulina Moreno <elizabeth.moreno.aguilera@gmail.com>
2018-01-03 16:45:34 -0800

    Update files with PEP8 requirements
    PEP8 requirements

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ud036_StarterCode
+Movie Trailer Website -----------------------------------------------------------------------------
+
 Source code for a Movie Trailer website.
+This project store a list of your favorite movies and tv_shows, including box art imagery and a trailer URL, and  allows visitors to browse their movies and tv_shows and watch the trailers.
+
+
+SETUP---------------------------------------------------------------------------------------------------
+1. Install python 2.7 ----- go to : https://www.python.org/download/releases/2.7/ for more information related to the specific OS.
+
+2. Download or clone de project from https://github.com/PaulinaMoreno/ud036_StarterCode
+
+3. Go to the project path and run
+			python entertainment_center.py
--- media.py
+++ media.py
@@ -5,40 +5,50 @@ Created on Wed Dec 22 17:10:27 2017
 @author: Paulina Moreno
 """
 import webbrowser
+
+
 class Video():
-    """ This Class provides a way to store movies and tv shows related information"""
-    
-    def __init__(self,title,duration,poster_image,trailer_url):
+
+    """ This Class provides a way to store
+     movies and tv shows related information"""
+    def __init__(self, title, duration, poster_image, trailer_url):
         print("Video constructor called")
         self.title = title
         self.duration = duration
-        self.poster_image_url= poster_image
+        self.poster_image_url = poster_image
         self.youtube_trailer_url = trailer_url
-        
+
     def show_trailer(self):
         webbrowser.open(self.youtube_trailer_url)
-        
+
+
 class Movie(Video):
-    """ This Class provides a way to store movie related information"""
-    VALID_RAITING = ["G","PG","PG-13","R"]
-    
-    def __init__(self,movie_title,movie_duration,movie_storyline,movie_poster_image,movie_trailer_url):
+
+    '''
+    This Class provides a way to store
+    movie related information
+    '''
+    VALID_RAITING = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, movie_title, movie_duration, movie_storyline,
+                 movie_poster_image, movie_trailer_url):
         print("Movie constructor called")
-        Video.__init__(self,movie_title,movie_duration,movie_poster_image,movie_trailer_url)
+        Video.__init__(self, movie_title, movie_duration,
+                       movie_poster_image, movie_trailer_url)
         self.storyline = movie_storyline
 
 
 class Tv_show(Video):
-    """ This Class provides a way to store tv show related information"""
-      
-    def __init__(self,tv_title,tv_duration,tv_season,tv_episode,tv_station,tv_poster_image,tv_trailer_url):
+
+    '''
+    This Class provides a way to store tv show
+    related information
+    '''
+    def __init__(self, tv_title, tv_duration, tv_season, tv_episode,
+                 tv_station, tv_poster_image, tv_trailer_url):
         print("Tv Show constructor called")
-        Video.__init__(self,tv_title,tv_duration,tv_poster_image,tv_trailer_url)
+        Video.__init__(self, tv_title, tv_duration,
+                       tv_poster_image, tv_trailer_url)
         self.tv_season = tv_season
         self.tv_episode = tv_episode
         self.tv_station = tv_station
-       
-
-        
-    
-
